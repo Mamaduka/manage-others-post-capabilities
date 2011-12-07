993bbb7aa4395cf44ec80058e4ae0f964b7177a8
George Mamadashvili <georgemamadashvili@gmail.com>
2011-12-07 12:33:29 +0400

    Minor changes

--- manage-others-post-capabilities.php
+++ manage-others-post-capabilities.php
@@ -5,18 +5,18 @@ Plugin URI:
 Description: Restrict users without capability to edit others posts see other uses posts
 Author: George Mamadashvili
 Version: 0.1
-Author URI: http://twitter.com/mamaduka
+Author URI: http://mamaduka.wordpress.com
 */
 
 add_filter( 'views_edit-post', 'mamaduka_mopc_edit_views' );
 add_filter( 'views_edit-page', 'mamaduka_mopc_edit_views' );
 add_action( 'pre_get_posts', 'mamaduka_mopc_set_query' );
 
-/*
- * 	If current user doesn't have capabilities to edit_others_posts
- *  Filter views on post edit screen
+/**
+ * If current user doesn't have capabilities to edit_others_posts
+ * Filter views on post edit screen
  * 
- *  @since 0.1
+ * @since 0.1
  */
 function mamaduka_mopc_edit_views( $views ) {
 	
@@ -24,14 +24,12 @@ function mamaduka_mopc_edit_views( $views ) {
 		unset( $views['all'] );
 		unset( $views['publish'] );
 		unset( $views['trash'] );
-		
-		return $views;
-	} else {
-		return $views;
 	}
+	
+	return $views;
 }
 
-/*
+/**
  * If current user doesn't have capabilities to edit_others_posts
  * Set wp_query to display only his/her posts 
  * 
@@ -39,8 +37,9 @@ function mamaduka_mopc_edit_views( $views ) {
  */
 function mamaduka_mopc_set_query( $wp_query ) {
 	global $current_user;
-    if ( is_admin() && !current_user_can( 'edit_others_posts' ) ) {
-        $wp_query->set( 'author', $current_user->ID );
-    }
+
+	if ( is_admin() && !current_user_can( 'edit_others_posts' ) ) {
+    	$wp_query->set( 'author', $current_user->ID );
+	}
 }
 ?>
